9fa4fdb2cef30923755a10126b2efdc8d134afe4
unknown <loc842211@gmail.com>
2025-06-03 10:24:30 +0700

    fix lai package

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,21 @@
-export const API_ROOT = 'http://localhost:3000'
+//export const API_ROOT = 'http://localhost:3000'
+
+const buildMode = import.meta.env.VITE_BUILD_MODE
+
+let apiRoot = ''
+if (buildMode === 'dev') {
+  apiRoot = 'http://localhost:3000'
+} else if (buildMode === 'production') {
+  apiRoot = 'https://fbt-shop-front-end.vercel.app'
+}
+
+export const API_ROOT = apiRoot
+
+//export const API_ROOT = 'https://trello-backend-wdel.onrender.com'
+
+
 
 export const DEFAULT_PAGE = 1
 export const DEFAULT_ITEMS_PER_PAGE = 12
+
+
